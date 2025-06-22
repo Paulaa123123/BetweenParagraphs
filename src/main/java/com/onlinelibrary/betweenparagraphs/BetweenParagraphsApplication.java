c5f7246faf8b37d630e749a78962a0c2be904715
paulajimenez <84386430+Paulaa123123@users.noreply.github.com>
2025-06-23 01:15:59 +0200

    Initial commit

---
+++ src/main/java/com/onlinelibrary/betweenparagraphs/BetweenParagraphsApplication.java
@@ -0,0 +1,13 @@
+package com.onlinelibrary.betweenparagraphs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetweenParagraphsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetweenParagraphsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinelibrary/betweenparagraphs/BetweenParagraphsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinelibrary.betweenparagraphs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetweenParagraphsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
